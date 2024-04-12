33398d7286dc7d5bc1fc0183ba9c052512c42349
Nyamisachela <nyamisachelagat@gmail.com>
2024-04-12 08:44:40 -0700

    Prints lines using a string and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 
-/*  a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop */
+//  a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop 
+
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
 let output = '';
+
 for (let i = 0; i < lines.length; i++) {
   output += lines[i] + '\n';
 }
